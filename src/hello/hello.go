bb6ec22c0e7207d752a33f9c035ca38a0de543fc
rkimmi <kimmirindel97@gmail.com>
2018-05-11 21:18:10 +1200

    another function, better naming

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,14 +3,21 @@ package main
 import ( 
     "fmt"
     "math"
+    "math/rand"
+
 )
 
 func main() {
     fmt.Println("hello, world\n")
     fmt.Println("The square root of 16 is", math.Sqrt(16))
-    sqrt()
+    square()
+    random()
 }
 
-func sqrt() {
+func square() {
     fmt.Println("The square root of 4 is", math.Sqrt(4))
+}
+
+func random() {
+    fmt.Println("Your random number is:", rand.Intn(100))
 }
