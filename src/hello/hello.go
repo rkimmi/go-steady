30ea45193e7aa5630ac29521c36c1ad246c0f4cb
rkimmi <kimmirindel97@gmail.com>
2018-05-11 21:40:15 +1200

    variables and floats

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,12 +6,15 @@ import (
     "math/rand"
 
 )
+var num1 float64 = 7.3
+var num2 float64 = 2.6
 
 func main() {
     fmt.Println("hello, world\n")
     fmt.Println("The square root of 16 is", math.Sqrt(16))
     square()
     random()
+    fmt.Println(add(num1, num2))
 }
 
 func square() {
@@ -19,5 +22,9 @@ func square() {
 }
 
 func random() {
-    fmt.Println("Your random number is:", rand.Intn(100))
+    fmt.Println("Your random number is:", rand.Intn(1000))
+}
+
+func add(x float64, y float64) float64 {
+    return x + y
 }
