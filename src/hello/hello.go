4c575d0b6cec5b9891eaad5215b543dc68466e6c
rkimmi <kimmirindel97@gmail.com>
2018-05-11 21:11:30 +1200

    some cute functions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import ( 
+    "fmt"
+    "math"
+)
 
 func main() {
     fmt.Println("hello, world\n")
+    fmt.Println("The square root of 16 is", math.Sqrt(16))
+    sqrt()
+}
+
+func sqrt() {
+    fmt.Println("The square root of 4 is", math.Sqrt(4))
 }
