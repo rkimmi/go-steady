71e6a770f967e4a07c6d247c68893a2742187afb
rkimmi <kimmirindel97@gmail.com>
2018-05-12 18:26:48 +1200

    float32, added notes

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -7,15 +7,17 @@ import (
 
 )
 var num1, num2 float64 = 7.3, 2.6 // declare variable outside func
+var num6, num7 float32 = 4.5, 3.2
+
+
 
 func main() {
     fmt.Println("hello, world\n")
     fmt.Println("The square root of 16 is", math.Sqrt(16))
     square()
     random()
-    num3, num4 := 3.7, 9.8 // declare var inside func - is guessing status as float, no need to specify
-    fmt.Println(add(num1, num2))
-    fmt.Println(add(num3, num4))
+    // num3, num4 := 3.7, 9.8 // declare var inside func - is guessing status as float64?, no need to specify
+    fmt.Println(add(num6, num7))
 }
 
 func square() {
@@ -26,6 +28,6 @@ func random() {
     fmt.Println("Your random number is:", rand.Intn(1000))
 }
 
-func add(x, y float64) float64 {
+func add(x, y float32) float32 {
     return x + y
 }
